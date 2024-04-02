8c0f9f3dfd65fac96e86d6d5c6b06a90ff31fbc1
Elliott Salvatori <elliott.salvatori@gmail.com>
2024-04-02 19:19:16 -0400

    initial commit

---
+++ Fitness Thing/Fitness_ThingApp.swift
@@ -0,0 +1,32 @@
+//
+//  Fitness_ThingApp.swift
+//  Fitness Thing
+//
+//  Created by Elliott Salvatori on 4/2/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Fitness_ThingApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
